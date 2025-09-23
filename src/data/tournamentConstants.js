6349ab795fc32e3cf95a709ef9fecea8cd04b911
Cheolkie Kim <ckiekim@naver.com>
2025-09-23 16:42:14 +0900

    Tournament 개선

--- src/data/tournamentConstants.js
+++ src/data/tournamentConstants.js
@@ -1,5 +1,5 @@
 export const tournamentCategories = ['남복', '여복', '혼복', '남단', '여단', '단체전'];
 export const tournamentOrganizers = ['KATO', 'KATA', 'KTA'];
-export const katoDivisions = ['국화부', '개나리부', '베테랑부', '마스터스부', '챌린저부'];
+export const katoDivisions = ['국화부', '개나리부', '베테랑부', '마스터스부', '챌린저부', '퓨처스부', '혼합복식부', '지역신인부', '지도자부'];
 export const kataDivisions = ['국화부', '개나리부', '베테랑부', '오픈부', '신인부'];
 export const ktaDivisions = ['국화부', '개나리부', '전국신인부', '지역신인부', '남자오픈부', '세미오픈부', '지도자부', '베테랑부'];
